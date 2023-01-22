a1a7fa283a5280b00f0587e31008cf1f4a398d8c
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2023-01-22 13:43:06 +0100

    generator training

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import torch
 from models import DiscriminatorCIFAR10, GeneratorCIFAR10
 from torch.optim import Adam
 from torch.nn import BCELoss
+import logging
 
 def train_model(
         device, 
@@ -17,6 +18,28 @@ def train_model(
     '''
     trains GAN model and saves its checkpoints to location
     given in checkpoints_dir.
+
+    Discriminator's training
+    -----------
+    ascending gradient of: log(D(x)) + log(1 - D(G(z)))
+
+    loss calculation split into two parts due to the form of BCEloss:
+    l = weight * ( label * log(x) + (1 - label) * log(1 - x) )
+
+    part_0 (for real images, label = 1):
+    l_0 = 1 * log(D(x)) + (1 - 1) * log(1 - D(G(z)))
+    l_0 = log(D(x))
+
+    part_1 (for noise, label = 0)
+    l_1 = 0 * log(D(x)) + (1 - 0) * log(1 - D(G(z)))
+    l_1 = log(1 - D(G(z)))
+
+    l_discriminator = l_0 + l_1 
+
+    Generator's training
+    ----------
+    Goal is to maximize D(G(z)). For simplification k steps (described in Algorithm 1 in GAN paper)
+    was set to 1 and noise samples were used from Discriminator training part.
 
     Parameters
     ----------
@@ -42,6 +65,9 @@ def train_model(
     dataset = CIFAR10GAN(f'{root_datasets_dir}/train/', class_name, train=True, download=download_datasets)
     loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
+    # number of observations
+    len_dataset = len(dataset)
+
     # models
     generator = GeneratorCIFAR10(latent_vector_length)
     discriminator = DiscriminatorCIFAR10()
@@ -54,25 +80,16 @@ def train_model(
     criterion = BCELoss()
 
     for epoch in range(n_epochs):
-        '''
-        Discriminator's training
-        ascending gradient of: log(D(x)) + log(1 - D(G(z)))
 
-        loss calculation split into two parts due to the form of BCEloss:
-        l = weight * ( label * log(x) + (1 - label) * log(1 - x) )
-
-        part_0 (for real images, label = 1):
-        l_0 = 1 * log(D(x)) + (1 - 1) * log(1 - D(G(z)))
-        l_0 = log(D(x))
-
-        part_1 (for noise, label = 0)
-        l_1 = 0 * log(D(x)) + (1 - 0) * log(1 - D(G(z)))
-        l_1 = log(1 - D(G(z)))
-
-        l_total = l_0 + l_1 
-        '''
         for id, batch in enumerate(loader, 0):
             
+            # calculated parameters
+            running_loss_discriminator = 0.0
+            running_loss_generator = 0.0
+
+            ##########################
+            # Discriminator's training
+            ##########################
             optimizer_discriminator.zero_grad()
 
             # inputs for discriminator and generator
@@ -90,7 +107,6 @@ def train_model(
             labels_fake_images = labels_fake_images.to(device)
 
             generated_images = generator(noise)
-            classified_generated_images = discriminator(generated_images)
             classified_real_images = discriminator(real_images)
 
             # part_0
@@ -101,12 +117,27 @@ def train_model(
             loss_1 = criterion(generated_images, labels_fake_images)
             loss_1.backward()
 
-            loss_total = loss_0 + loss_1
+            loss_discriminator = loss_0 + loss_1
             optimizer_discriminator.step()
 
+            ##########################
+            # Generator's training
+            ##########################
+            optimizer_generator.zero_grad()
 
-            
+            generated_images = generator(noise)
+            classified_generated_images = discriminator(generated_images)
+            loss_generator = criterion(classified_generated_images, labels_fake_images)
 
+            loss_generator.backward()
+            optimizer_generator.step()
 
+            # iteration statistics
+            running_loss_discriminator += loss_discriminator.item()
+            running_loss_generator += loss_generator.item()
 
-        pass
+        # epoch statistics
+        epoch_loss_discriminator = round(running_loss_discriminator / len_dataset, 2)
+        epoch_loss_generator = round(running_loss_generator / len_dataset, 2)
+
+        logging.info(f"Epoch: {epoch}, loss_disciminator: {epoch_loss_discriminator}, loss_generator: {epoch_loss_generator}")
