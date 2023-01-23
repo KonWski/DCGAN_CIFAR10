eed9f346ef9031e3d2b9ffbaa036fb4c43cf99c1
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2023-01-23 19:04:34 +0100

    fix

--- train.py
+++ train.py
@@ -97,10 +97,10 @@ def train_model(
             noise = torch.rand(batch_size, latent_vector_length)
 
             # labels
-            tensor_zeros = torch.full((batch_size, 1), 0)
-            tensor_ones = torch.full((batch_size, 1), 1)
-            labels_real_images = torch.cat((tensor_zeros, tensor_ones), dim=1, dtype=torch.float)
-            labels_fake_images = torch.cat((tensor_ones, tensor_zeros), dim=1, dtype=torch.float)
+            tensor_zeros = torch.full((batch_size, 1), 0, dtype=torch.float)
+            tensor_ones = torch.full((batch_size, 1), 1, dtype=torch.float)
+            labels_real_images = torch.cat((tensor_zeros, tensor_ones), dim=1)
+            labels_fake_images = torch.cat((tensor_ones, tensor_zeros), dim=1)
 
             # send tensors to device
             real_images = real_images.to(device)
