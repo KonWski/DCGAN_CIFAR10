2c890074547967d9a0afcddcfa4756936d218931
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2023-02-03 17:24:12 +0100

    dataset

--- dataset.py
+++ dataset.py
@@ -1,19 +1,17 @@
 from torchvision.datasets import CIFAR10
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, Compose
 
 class CIFAR10GAN(CIFAR10):
 
     def __init__(self, 
             root: str, 
             class_name: str,
-            train: bool = True, 
+            train: bool = True,
+            transform: Compose = Compose([ToTensor()])
             download: bool = False,
         ) -> None:
         '''
         Dataset limiting original CIFAR10 to specified class_name
-
-        Full path to dataset is defined by:
-        {self.root_dir}/{self.state}/stanford_cars/cars_{self.state}
 
         Attributes
         ----------
@@ -21,10 +19,15 @@ class CIFAR10GAN(CIFAR10):
             limits records by given class name f.e. cat
         root_dir: str
             path to CIFAR10 dataset content
-        state: str
-            train or test
+        train: bool = True
+            select train or test part of CIFAR10
+        transform: Compose
+            set of transformation performed on images
+        download: bool = False
+            download dataset from torchvision or used existing data located
+            in root_dir folder
         '''
-        super().__init__(root = root, train = train, transform = ToTensor(), download = download)
+        super().__init__(root = root, train = train, transform = transform, download = download)
         self.class_name = class_name
         self.class_id = self.class_to_idx[self.class_name]
         self.data, self.targets = self._filter_by_class_name()
